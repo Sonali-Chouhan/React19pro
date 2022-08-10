5f506191ed6c693cf5602b82945b80734fde3e28
Sonali-Chouhan <sonaliyuvasoft363@gmail.com>
2022-08-10 10:33:59 +0530

    react-19-prject

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import {getApi} from "so-redux/dist/Redux/Action/Action";
-import { useDispatch } from 'react-redux/es/hooks/useDispatch';
+import { useEffect } from "react";
+import { useDispatch,useSelector } from "react-redux/es/exports";
+import { getApi } from 'for_redux/dist/Redux/Action/Action'
 const App = () => {
+   const dispatch = useDispatch();
+   const state = useSelector(state => state.reducer.List);
+   console.log("Gg",state);
+  useEffect(() => {
+    dispatch(getApi());
+  }, [])
 
   return (
     <div>App</div>
